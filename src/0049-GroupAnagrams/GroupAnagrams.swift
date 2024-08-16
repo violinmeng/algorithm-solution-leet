a8a996f42e2a1c871130a5a69fc65c4e8900b552
Elin Wang <violinmeng@gmail.com>
2024-08-16 11:11:59 +0800

    add 0049 group anagrams yet another solution

--- src/0049-GroupAnagrams/GroupAnagrams.swift
+++ src/0049-GroupAnagrams/GroupAnagrams.swift
@@ -1,3 +1,10 @@
+// The key to solve this problem is determining how to identify whether two strings are anagrams
+// The intuitive way is convert the strings to an unified form, and if the two strings have the same form, they are anagrams
+// so we can transform them by sorting each of the strings in alphabet order.
+// when we consider the sort algorithm, the counting algorithm will be more efficiency in this case.
+// so record the count of each character in every string,
+// and compare the count of each alphabet.
+// this can be another way of solve this problem.
 
 func groupAnagrams(_ strs: [String]) -> [[String]] {
     var map: [String:[String]] = [:]
@@ -11,4 +18,32 @@ func groupAnagrams(_ strs: [String]) -> [[String]] {
     return Array(map.values)
 }
 
+func groupAnagrams2(_ strs: [String]) -> [[String]] {
+    if strs.count <= 1 {
+        return [strs]
+    }
+    var map: [String: [String]] = [:]
+    for str in strs {
+        var countArr = Array(repeating: 0, count: 26)
+        for c in str {
+            let index = c.asciiValue! - Character("a").asciiValue!
+            countArr[Int(index)] += 1
+        }
+
+        var key = ""
+        for i in 0..<26 {
+            if countArr[i] > 0 {
+                let c = Character(UnicodeScalar(UInt8(i) + Character("a").asciiValue!))
+                key += String(c) + String(countArr[i])
+            }
+        }
+        
+        var val = map[key] ?? []
+        val.append(str)
+        map[key] = val
+    }
+    return Array(map.values)
+}
+
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams2(["eat","tea","tan","ate","nat","bat"]))
