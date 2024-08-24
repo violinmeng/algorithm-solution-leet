eece917a2e54d37afee0b7cc279b87b49e12a794
Elin Wang <violinmeng@gmail.com>
2024-08-24 23:19:46 +0800

    add 3069 result array

---
+++ src/3069-ResultArray/ResultArray.rs
@@ -0,0 +1,27 @@
+
+pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
+    if nums.len() < 3 {
+        return nums;
+    }
+    let mut arr1: Vec<i32> = Vec::new();
+    let mut arr2: Vec<i32> = Vec::new();
+    arr1.push(nums[0]);
+    arr2.push(nums[1]);
+
+    for num in &nums[2..] {
+        if arr1.last() > arr2.last() {
+            arr1.push(*num);
+        } else {
+            arr2.push(*num);
+        }
+    }
+
+    arr1.append(&mut arr2);
+    return arr1;
+}
+
+fn main() {
+    let nums = vec![2,1,3];
+    let result = result_array(nums);
+    println!("result: {:?}", result);
+}
