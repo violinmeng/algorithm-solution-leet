7480aa14e1e4a3d30ffad8de26ef95905db9ddc0
Elin Wang <violinmeng@gmail.com>
2024-08-26 12:03:23 +0800

    add 0698 optimized v2 solution

--- src/0698-PartitionSubsets/PartitionSubsets.rs
+++ src/0698-PartitionSubsets/PartitionSubsets.rs
@@ -1,5 +1,6 @@
-pub fn can_partition_k_subsets(nums: Vec<i32>, k: i32) -> bool {
+use std::time::Instant;
 
+pub fn can_partition_k_subsets(nums: Vec<i32>, k: i32) -> bool {
     let sum = nums.iter().sum::<i32>();
     if sum % k != 0 {
         return false;
@@ -39,7 +40,27 @@ pub fn can_partition_k_subsets(nums: Vec<i32>, k: i32) -> bool {
     return dfs((1 << n) - 1, 0, target, &mut dp, &nums);
 }
 
-pub fn can_partition_k_subsets_slow(nums: Vec<i32>, k: i32) -> bool {
+pub fn can_partition_k_subsets_v2(mut nums: Vec<i32>, k: i32) -> bool {
+    fn back_trace(nums: &mut Vec<i32>, buckets: &mut Vec<i32>, start: i32, target: i32) -> bool {
+        if start < 0 { return true; }
+        let num = nums[start as usize];
+        for i in 0..buckets.len() {
+            if num + buckets[i] <= target {
+                buckets[i] += num;
+                if back_trace(nums, buckets, start - 1, target) { return true; }
+                buckets[i] -= num;
+            }
+            if buckets[i] == 0 { break; }
+        }
+        false
+    }
+    let (sum, n) = (nums.iter().sum::<i32>(), nums.len() as i32);
+    nums.sort();
+    if sum % k != 0 || sum / k < nums[0] || sum / k < nums[n as usize - 1] { return false; }
+    back_trace(&mut nums, &mut vec![0; k as usize], n - 1, sum / k)
+}
+
+pub fn can_partition_k_subsets_no_cache(nums: Vec<i32>, k: i32) -> bool {
 
     let sum = nums.iter().sum::<i32>();
     if sum % k != 0 {
@@ -82,4 +103,18 @@ fn test() {
     assert_eq!(can_partition_k_subsets(vec![4, 3, 2, 3, 5, 2, 1], 4), true);
     assert_eq!(can_partition_k_subsets(vec![1, 2, 3, 4], 3), false);
     assert_eq!(can_partition_k_subsets(vec![1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5], 5), true);
+}
+
+fn main() {
+    let start = Instant::now();
+    for _i in 0..10000 {
+        can_partition_k_subsets(vec![1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5], 5);
+    }
+    println!("cost: {:#?}", start.elapsed());
+
+    let start = Instant::now();
+    for _i in 0..10000 {
+        can_partition_k_subsets_v2(vec![1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5], 5);
+    }
+    println!("cost: {:#?}", start.elapsed());
 }
