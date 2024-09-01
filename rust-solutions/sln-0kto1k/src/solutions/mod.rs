081d2a4c4ae0209a72c0d7831e680a14209daf96
Elin Wang <violinmeng@gmail.com>
2024-09-01 22:17:29 +0800

    add 0042 trapping rain water another two solutions

--- rust-solutions/sln-0kto1k/src/solutions/mod.rs
+++ rust-solutions/sln-0kto1k/src/solutions/mod.rs
@@ -1,6 +1,8 @@
 
 mod s0042_trapping_rain_water;
 pub use s0042_trapping_rain_water::trap;
+pub use s0042_trapping_rain_water::trap_dp;
+pub use s0042_trapping_rain_water::trap_stack;
 
 mod s0690_get_employee_importance;
 pub use s0690_get_employee_importance::get_importance;
--- rust-solutions/sln-0kto1k/src/solutions/s0042_trapping_rain_water.rs
+++ rust-solutions/sln-0kto1k/src/solutions/s0042_trapping_rain_water.rs
@@ -24,6 +24,55 @@ pub fn trap(height: Vec<i32>) -> i32 {
     }
     res
 }
+// Dynamic Programming
+pub fn trap_dp(height: Vec<i32>) -> i32 {
+    let len = height.len();
+    if len == 0 {
+        return 0;
+    }
+
+    let left_max = height.iter().fold(Vec::new(), |mut a: Vec<i32> , b| {
+        if let Some(x) = a.last() {
+            a.push(*x.max(b));
+        } else {
+            a.push(*b);
+        }
+        a
+    });
+
+    let mut right_max = height.iter().rfold(Vec::new(), |mut acc: Vec<i32>,  b | {
+        if let Some(x) = acc.last() {
+            acc.push(*x.max(b));
+        } else {
+            acc.push(*b);
+        }
+        (move|| acc)()
+    });
+    right_max.reverse();
+
+    let min_left_right = left_max.iter().zip(right_max.iter()).map(|(x, y)| x.min(y));
+    let ans = min_left_right.zip(height.iter()).map(|(x, y)| x - y).sum();
+    return ans;
+}
+
+// Monotone Stack
+pub fn trap_stack(height: Vec<i32>) -> i32 {
+    let mut stack = Vec::new();
+    let mut ans = 0;
+    for i in 0..height.len() {
+        while !stack.is_empty() && height[i] > height[*stack.last().unwrap()] {
+            let top = stack.pop().unwrap();
+            if stack.is_empty() {
+                break;
+            }
+            let distance = (i - stack.last().unwrap() - 1) as i32;
+            let bounded_height = height[*stack.last().unwrap()].min(height[i]) - height[top];
+            ans += distance * bounded_height;
+        }
+        stack.push(i);
+    }
+    ans
+}
 
 #[cfg(test)]
 mod tests {
@@ -33,5 +82,13 @@ mod tests {
         assert_eq!(trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
         assert_eq!(trap(vec![4, 2, 0, 3, 2, 5]), 9);
         assert_eq!(trap(vec![4, 2, 3]), 1);
+    }
+    #[test]
+    fn test_trap_dp() {
+        assert_eq!(trap_dp(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
+    }
+    #[test]
+    fn test_trap_stack() {
+        assert_eq!(trap_stack(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
     }
 }
